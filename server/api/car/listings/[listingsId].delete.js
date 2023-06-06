415261964bf5b12fcdfa2879aa189d67e38586a5
Ridwan Naibi Suleiman <rid.sul.nai@gmail.com>
2023-06-06 18:07:56 +0100

    Added database transactions feature

---
+++ server/api/car/listings/[listingsId].delete.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+
+export default defineEventHandler(async (event) => {
+
+
+
+    const { listingsId } = event.context.params;
+
+    return prisma.car.delete({
+        where: {
+            id: parseInt(listingsId),
+        },
+    });
+
+});
